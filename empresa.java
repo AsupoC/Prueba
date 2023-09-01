e913c99b40898bc00c94296c61c6826ceede4391
Alberto Supo <asupo@une.edu.pe>
2023-09-01 07:12:40 -0500

    Version inicial

---
+++ empresa.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class empresa {
+    public static void main(String[] args) {
+        Scanner imput = new Scanner(System.in);
+        System.out.println("Ingresa un numero: ");
+        int valor = imput.nextInt();
+        System.out.println("El numero ingresado es: " + valor);
+    }
+}
